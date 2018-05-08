e71939450794bf5a5738951dcb57e8ad120a4529
Robert SAY <robert.saythathongsak@hetic.net>
2018-05-08 11:29:41 +0200

    Test Express JS

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+ 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+ 
+const sql_connect = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'twittanalyze'
+});
+ 
+sql_connect.connect();
+ 
+app.get('/', function (req, res) {
+    return res.send({ error: true, message: 'hello' })
+});
+ 
+app.get('/users', function (req, res) {
+    sql_connect.query('SELECT * FROM user', function (error, results, fields) {
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'Get all users.' });
+    });
+});
+ 
+app.get('/user/:id', function (req, res) { 
+    let user_id = req.params.id;
+
+    sql_connect.query('SELECT * FROM user where id=?', user_id, function (error, results, fields) {
+        if (error) throw error;
+        return res.send({ error: false, data: results[0], message: 'Get one user information.' });
+    });
+});
+
+app.get('/user/:id/tweet', function (req, res) { 
+    let user_id = req.params.id;
+
+    sql_connect.query('SELECT * FROM user INNER JOIN tweets WHERE user.id = tweets.id_user AND user.id=?', user_id, function (error, results, fields) {
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'Get tweets of one user.' });
+    });
+});
+
+app.all("*", function (req, res) {
+    return res.status(404).send('page not found')
+});
+ 
+app.listen(8080, function () {
+	console.log('Node app is running on port 8080');
+});
