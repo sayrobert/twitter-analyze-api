dac4a486a754123cbc2a6f8f5698baa663c1465a
Robert SAY <robert.saythathongsak@hetic.net>
2018-05-20 15:10:51 +0200

    Test views

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+app.set('view engine', 'pug');
  
 const sql_connect = mysql.createConnection({
     host: 'localhost',
@@ -34,6 +36,177 @@ app.get('/', function (req, res) {
     return res.send({ data: results, message: 'Get all routes' })
 });
  
+
+app.get('/front/users', function (req, res) {
+
+    var personList = [];
+
+    sql_connect.query('SELECT * FROM user', function (error, results, fields) {
+        if (error) { throw error; }
+        else {
+            for (var i = 0; i < results.length; i++) {
+
+                // Create an object to save current row's data
+                var person = {
+                    'name':results[i].name,
+                    'pseudo':results[i].pseudo,
+                    'id':results[i].id
+                }
+                // Add object into array
+                personList.push(person);
+            }
+            res.render('users', {"personList": personList});
+        }
+    });
+});
+ 
+app.get('/front/user/:id', function (req, res) {
+    let hostname = req.protocol + '://' + req.get('host');
+    let user_id = req.params.id;
+    let exemple_id = 1976143068;
+
+    sql_connect.query('SELECT * FROM user where id=?', user_id, function (error, results, fields) {
+       if (error) { throw error; }
+        else {
+            if(results.length==1) {
+                // Create the object to save the data.
+                var person = {
+                    'name':results[0].name,
+                    'pseudo':results[0].pseudo,
+                    'id':results[0].id
+                }
+                // render the details.plug page.
+                res.render('detail-user', {"person": person});
+            } else {
+                // render not found page
+                res.status(404).json({"status_code":404, "status_message": "Not found"});
+            }
+        }
+    });
+});
+
+app.get('/front/user/:id/tweets', function (req, res) {
+
+    var tweetList = [];
+
+    let hostname = req.protocol + '://' + req.get('host');
+    let user_id = req.params.id;
+    let exemple_id = 1976143068;
+
+    sql_connect.query('SELECT * FROM tweets INNER JOIN user ON tweets.id_user = user.id AND user.id=?', user_id, function (error, results, fields) {
+        if (error) { throw error; }
+        else {
+            for (var i = 0; i < results.length; i++) {
+
+                // Create an object to save current row's data
+                var tweets = {
+                    'text':results[i].text
+                }
+                // Add object into array
+                tweetList.push(tweets);
+            }
+            res.render('user-tweets', {"tweetList": tweetList});
+        }
+    });
+});
+
+app.get('/front/popularhashtags', function (req, res) {
+
+    var hashtagsList = [];
+    sql_connect.query('SELECT content, COUNT(*) count FROM `hashtags` GROUP BY content ORDER BY count DESC LIMIT 10', function (error, results, fields) {
+        if (error) { throw error; }
+        else {
+            for (var i = 0; i < results.length; i++) {
+
+                // Create an object to save current row's data
+                var hashtag = {
+                    'content':results[i].content
+                }
+                // Add object into array
+                hashtagsList.push(hashtag);
+            }
+            res.render('hashtags', {"hashtagsList": hashtagsList});
+        }
+    });
+});
+
+app.get('/front/historytags', function (req, res) {
+
+    var hashtagsList = [];
+
+    sql_connect.query('SELECT hashtag_content, month FROM hashtags_history', function (error, results, fields) {
+        if (error) { throw error; }
+        else {
+            for (var i = 0; i < results.length; i++) {
+
+                // Create an object to save current row's data
+                var hashtag = {
+                    'content':results[i].hashtag_content,
+                    'month':results[i].month
+                }
+                // Add object into array
+                hashtagsList.push(hashtag);
+            }
+            res.render('history-hashtags', {"hashtagsList": hashtagsList});
+        }  
+    });
+});
+
+app.get('/front/historytags/:month', function (req, res) {
+
+    var hashtagsList = [];
+
+    let hostname = req.protocol + '://' + req.get('host');
+    let month_exemple = "052018";
+    let hashtag_month = req.params.month;
+
+    sql_connect.query('SELECT hashtag_content, month FROM hashtags_history WHERE month=?', hashtag_month, function (error, results, fields) {
+        if (error) { throw error; }
+        else {
+            for (var i = 0; i < results.length; i++) {
+
+                // Create an object to save current row's data
+                var hashtag = {
+                    'content':results[i].hashtag_content,
+                    'month':results[i].month
+                }
+                // Add object into array
+                hashtagsList.push(hashtag);
+            }
+            res.render('history-hashtags-month', {"hashtagsList": hashtagsList});
+        }
+    });
+});
+
+app.get('/front/:hashtag/:pseudo', function (req, res) {
+
+    let hostname = req.protocol + '://' + req.get('host');
+    let hashtag = req.params.hashtag;
+    let pseudo = req.params.pseudo;
+
+    let hashtag_exemple = "Karlspreis";
+    let pseudo_exemple = "emmanuelmacron"; 
+
+    sql_connect.query('SELECT COUNT(*) AS nombredetweets, h.content, u.pseudo FROM tweets t INNER JOIN hashtags h ON t.id = h.id_tweet INNER JOIN user u ON t.id_user = u.id WHERE h.content = ? AND u.pseudo = ?', [hashtag, pseudo], function (error, results, fields) {
+        if (error) { throw error; }
+        else {
+            if(results.length==1) {
+                // Create the object to save the data.
+                var count = {
+                    'count':results[0].nombredetweets,
+                    'content':results[0].content,
+                    'pseudo':results[0].pseudo
+                }
+                // render the details.plug page.
+                res.render('hashtag-user', {"count": count});
+            } else {
+                // render not found page
+                res.status(404).json({"status_code":404, "status_message": "Not found"});
+            }
+        }
+    });
+});
+
 app.get('/users', function (req, res) {
     sql_connect.query('SELECT * FROM user', function (error, results, fields) {
         if (error) throw error;
