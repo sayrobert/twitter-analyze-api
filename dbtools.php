1b82436ae74ee67bc4217de8830e2876958a27c6
Robert SAY <robert.saythathongsak@hetic.net>
2018-05-27 16:41:54 +0200

    Test

--- dbtools.php
+++ dbtools.php
@@ -5,7 +5,7 @@
 
 		if (empty($link))
 
-		$link = mysqli_connect('localhost', 'root', 'root', 'twittanalyze') or die (mysqli_connect_error());
+		$link = mysqli_connect('bnk4bi09l-mysql.services.clever-cloud.com', 'ukyzduvz8tlth9yz', 'iE8SxlzfKwiz3WobuIf', 'bnk4bi09l') or die (mysqli_connect_error());
 		mysqli_set_charset($link, "utf8");
 		$result = mysqli_query($link, $query) or die (mysqli_error($link));
 		return $result;
